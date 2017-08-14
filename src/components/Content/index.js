e4ecb1970036bf52d8206724f951b8286b2ca0f7
yyao <yaoyuan526@gmail.com>
2017-08-14 02:45:59 -0700

    removed unused line

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -3,7 +3,6 @@ import Cover from './components/Cover';
 import About from './components/About';
 import Education from './components/Education';
 import Projects from './components/Projects';
-import Parallax from 'react-springy-parallax';
 import './Content.css'
 
 class Content extends Component {
